b3f5ae1957299b0c785a89817520c7f0e57e51f1
Jems <jamaludinsalam11@gmail.com>
2018-12-28 14:55:31 +0700

    Modifiedn cart controller

--- resources/views/layouts/layout2.blade.php
+++ resources/views/layouts/layout2.blade.php
@@ -46,7 +46,7 @@
 					
 					<!-- Logo desktop -->		
 					<a href="/" class="logo">
-						<img src="{{asset('front/front2/images/icons/coffee-logo.png')}}" alt="IMG-LOGO">
+						<img src="{{secure_asset('front/front2/images/icons/coffee-logo.png')}}" alt="IMG-LOGO">
 					</a>
 
 					<!-- Menu desktop -->
@@ -108,7 +108,7 @@
 		<div class="wrap-header-mobile">
 			<!-- Logo moblie -->		
 			<div class="logo-mobile">
-				<a href="{{route('home')}}"><img src="{{asset('front/front2/images/icons/coffee-logo.png')}}" alt="IMG-LOGO"></a>
+				<a href="{{route('home')}}"><img src="{{secure_asset('front/front2/images/icons/coffee-logo.png')}}" alt="IMG-LOGO"></a>
 			</div>
 
 			<!-- Icon header -->
@@ -170,7 +170,7 @@
 		<div class="modal-search-header flex-c-m trans-04 js-hide-modal-search">
 			<div class="container-search-header">
 				<button class="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
-					<img src="{{asset('front/front2/images/icons/icon-close2.png')}}" alt="CLOSE">
+					<img src="{{secure_asset('front/front2/images/icons/icon-close2.png')}}" alt="CLOSE">
 				</button>
 
 				<form class="wrap-search-header flex-w p-l-15">
